e68f942c1b10548725569dbaa8d57a5db69fcc13
Samarovve <samarovve@gmail.com>
2026-04-24 14:09:23 +0300

    Update Particle.cpp

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -73,21 +73,13 @@ private:
     double sigma;
     double sigma6;
     double sigma12;
+    double mass;
+    double U_cut;
 
 public:
 
     ParticleSystem(int count, double mass, double boxSize, double v_max, double epsilon, double sigma) {
-
-        this->boxSize = boxSize;
-        this->particles = createParticles(count, mass, boxSize, v_max);
-        this->cutoff = boxSize * 0.99;
-        this->epsilon = epsilon;
-        this->sigma = sigma;
-
-        double sigma2 = this->sigma * this->sigma;
-        this->sigma6 = sigma2 * sigma2 * sigma2;
-        this->sigma12 = this->sigma6 * this->sigma6;
-
+        this->initParticles(count, mass, boxSize, v_max, epsilon, sigma);
     }
 
     void resetForces() {
@@ -102,20 +94,10 @@ public:
         double cutoff2 = this->cutoff * this->cutoff;
 
 
-        for (int i = 0; i < particles.size(); i++) {
-            for (int j = i + 1; j < particles.size(); j++) {
+        for (int i = 0; i < this->particles.size(); i++) {
+            for (int j = i + 1; j < this->particles.size(); j++) {
 
-                Vec3 r = particles[i].position - particles[j].position;
-
-                // --- Minimum image convention (PBC) ---
-                if (r.x >  0.5 * this->boxSize) r.x -= this->boxSize;
-                if (r.x < -0.5 * this->boxSize) r.x += this->boxSize;
-
-                if (r.y >  0.5 * this->boxSize) r.y -= this->boxSize;
-                if (r.y < -0.5 * this->boxSize) r.y += this->boxSize;
-
-                if (r.z >  0.5 * this->boxSize) r.z -= this->boxSize;
-                if (r.z < -0.5 * this->boxSize) r.z += this->boxSize;
+                Vec3 r = computeSeparation(i, j);
 
                 double r2 = r.lengthSq();
 
@@ -166,17 +148,97 @@ public:
         this->cutoff = boxSize * 0.99;
         this->epsilon = epsilon;
         this->sigma = sigma;
+        this->mass = mass;
 
         double sigma2 = this->sigma * this->sigma;
         this->sigma6 = sigma2 * sigma2 * sigma2;
         this->sigma12 = this->sigma6 * this->sigma6;
+
+        double inv_rcut2 = 1.0 / (this->cutoff * this->cutoff);
+        double inv_rcut6 = inv_rcut2 * inv_rcut2 * inv_rcut2;
+        double inv_rcut12 = inv_rcut6 * inv_rcut6;
+
+        this->U_cut = (this->sigma12 * inv_rcut12 - this->sigma6 * inv_rcut6);
+
+        Vec3 v_avg = computeAverageVelocity();
+
+        for (auto &p: this->particles) {
+            p.velocity -= v_avg;
+        }
+    }
+
+    Vec3 computeAverageVelocity() const {
+
+        Vec3 velocity_avg(0, 0, 0);
+
+        for (auto &p: this->particles) {
+            velocity_avg += p.velocity;
+        }
+
+        return velocity_avg / this->particles.size();
+    }
+
+    Vec3 computeSeparation(int i, int j) const {
+
+        Vec3 r = this->particles[i].position - this->particles[j].position;
+
+        // --- Minimum image convention (PBC) ---
+        if (r.x >  0.5 * this->boxSize) r.x -= this->boxSize;
+        if (r.x < -0.5 * this->boxSize) r.x += this->boxSize;
+
+        if (r.y >  0.5 * this->boxSize) r.y -= this->boxSize;
+        if (r.y < -0.5 * this->boxSize) r.y += this->boxSize;
+
+        if (r.z >  0.5 * this->boxSize) r.z -= this->boxSize;
+        if (r.z < -0.5 * this->boxSize) r.z += this->boxSize;
+
+        return r;
     }
 
 
     // --- Energy / diagnostics ---
-    double kineticEnergy() const;
-    double potentialEnergy() const;
-    double temperature() const;
+    double kineticEnergy() const {
+        double velocity_total_Sq = 0;
+
+        for (auto &p: this->particles) {
+            velocity_total_Sq += p.velocity.lengthSq();
+        }
+
+        return velocity_total_Sq * this->mass * 0.5;
+    }
+
+
+        double potentialEnergy() const {
+
+        double cutoff2 = this->cutoff * this->cutoff;
+        double U_p = 0;
+
+        for (int i = 0; i < this->particles.size(); i++) {
+            for (int j = i + 1; j < this->particles.size(); j++) {
+
+                Vec3 r = computeSeparation(i, j);
+
+                double r2 = r.lengthSq();
+
+                // --- cutoff ---
+                if (r2 > cutoff2 || r2 == 0.0) continue;
+
+                double inv_r2 = 1.0 / r2;
+                double inv_r6 = inv_r2 * inv_r2 * inv_r2;
+                double inv_r12 = inv_r6 * inv_r6;
+
+
+                U_p += this->sigma12 * inv_r12 - this->sigma6 * inv_r6 - this->U_cut;
+
+            }
+        }
+
+        return 4 * this->epsilon * U_p;
+    }
+
+    double temperature() const {
+        return 2.0 / 3.0 * this->kineticEnergy() / (double)(this->particles.size() - 1);
+    }
 
     // --- Access ---
     std::vector<Particle>& getParticles();
